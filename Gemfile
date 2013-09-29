ce38e54df6ebba29864ab1dee0ae6b3e64d246cb
makoto_kw <makoto.kw@gmail.com>
2013-09-29 22:59:42 +0900

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'jekyll'
+gem 'jekyll-assets'
+
+# for Markdown
+gem 'redcarpet'
+
+# for Sass on jekyll-assets
+gem 'compass'
+
+gem 'stringex', '~> 1.4.0'
---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        bower: {
+            install: {
+                options: {
+                    targetDir: './source/_assets/components',
+                    cleanTargetDir: true,
+                    layout: function(type, component) {
+                        var renamedType = type;
+                        if (type == 'js') renamedType = 'javascripts';
+                        else if (type == 'css') renamedType = 'stylesheets';
+                        else if (type == 'font') renamedType = 'fonts';
+
+                        var renamedComponent = component;
+                        if (component == 'sass-bootstrap') renamedComponent = 'bootstrap';
+
+                        return path.join(renamedType, renamedComponent);
+                    }
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.registerTask('default', []);
+};
