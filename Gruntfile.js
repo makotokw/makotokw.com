5bfe2fecd8a58efdc3ad4bab7bd16a16f9734a64
makoto_kw <makoto.kw@gmail.com>
2013-12-26 15:48:37 +0900

    makotokw2014 theme Updated FontAwesome 4.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,5 +23,5 @@ module.exports = function (grunt) {
         }
     });
     grunt.loadNpmTasks('grunt-bower-task');
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', ['bower']);
 };
