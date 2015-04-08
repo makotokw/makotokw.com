0f76b6740eab7e1687cd3c7cf24937f9c3feb7d9
makoto_kw <makoto.kw@gmail.com>
2015-04-09 03:17:30 +0900

    Images of post

---
+++ source/assets/site/images/2009/dark/style.js
@@ -0,0 +1,13 @@
+{
+	padding:[0,0,0,0],
+	titleColor:['#3399ff','#000000'],
+	textColor:['#FFFFFF','#000000'],
+	headerColor:['#CAE0DD','#000000'], //'#80C6CC',
+	remainColor:['#FFFF00','#000000'],
+	emptyItemColor:['#808080','#000000'],
+	completedItemColor:['#909090','#000000'],
+	justbeforeItemColor:['#FFFF00','#000000'],
+	currentItemColor:['#FFFF00','#000000'],
+	apptIconColor:['#3399ff'], 
+	taskIconColor:['#999999','#0000ff','#ffffff','#ff0000'] // Done,Low,Normal,High for Task
+}
