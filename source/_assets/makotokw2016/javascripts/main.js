dd3e19a7628fc60b7d98841e69f1e8826e931065
makoto_kw <makoto.kw@gmail.com>
2016-07-24 11:08:46 +0900

    Fixed redirect in top page

--- source/_assets/makotokw2016/javascripts/main.js
+++ source/_assets/makotokw2016/javascripts/main.js
@@ -36,7 +36,7 @@ window.makotokw = {
         };
     }
 
-    function redirectPage(dest){
+    function redirectPage(dest) {
         if (window.location.replace) {
             window.location.replace(dest);
         } else {
@@ -44,26 +44,35 @@ window.makotokw = {
         }
     }
 
-    // content negotiation on top page
-    var referer = document.referrer || '';
-    if ($.type(referer) === 'string') {
-        if (referer.indexOf(document.location.origin) !== 0) {
-            var lang = navigator.language || navigator.userLanguage;
-            var langCodes = ['ja'];
-            for (var i = 0; i < langCodes.length; i++) {
-                if (lang.substr(0, 2) === langCodes[i]) {
-                    var pathname = document.location.pathname || '';
-                    if ($.type(pathname) === 'string') {
-                        if (pathname.match(new RegExp('^/' + lang + '(/|$)'))) {
-                            break;
+    function redirectPageByContentNegotiation() {
+        // content negotiation on top page
+        var referer = document.referrer;
+        if ($.type(referer) === 'string' && referer.length > 0) {
+            if (!document.location || !document.location.origin) {
+                return;
+            }
+            // redirect only a referer is a external url
+            if (referer.indexOf(document.location.origin) !== 0) {
+                var userLanguage = navigator.language || navigator.userLanguage;
+                var langCodes = ['ja'];
+                for (var i = 0; i < langCodes.length; i++) {
+                    var langCode = langCodes[i];
+                    if (userLanguage.substr(0, 2) === langCodes[i]) {
+                        var pathname = document.location.pathname || '';
+                        if ($.type(pathname) === 'string') {
+                            if (pathname.match(new RegExp('^/' + langCode + '(/|$)'))) {
+                                return;
+                            }
                         }
+                        redirectPage('/' + langCodes[i] + pathname + document.location.search + document.location.hash);
+                        return;
                     }
-                    redirectPage('/' + langCodes[i] + pathname + document.location.search + document.location.hash);
-                    break;
                 }
             }
         }
     }
+
+    redirectPageByContentNegotiation();
 
     /*jshint camelcase: false */
     // remove hash http://support.addthis.com/customer/portal/questions/352733-how-to-remove-hash-from-url-
