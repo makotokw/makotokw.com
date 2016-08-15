75b5dd8a3ba122583822cb1f69225541246bd423
makoto_kw <makoto.kw@gmail.com>
2016-08-15 10:43:50 +0900

    Fixed navbar by using Headroom.js

--- source/_assets/makotokw2016/javascripts/vendor.js
+++ source/_assets/makotokw2016/javascripts/vendor.js
@@ -1,4 +1,6 @@
 //= require jquery.easing/js/jquery.easing
+//= require headroom.js/js/headroom
+//= require headroom.js/js/jQuery.headroom
 //= require isotope/js/isotope.pkgd
 //= require underscore/js/underscore
 //= require underscore.string/js/underscore.string
--- source/_assets/makotokw2016/javascripts/views/stage.js
+++ source/_assets/makotokw2016/javascripts/views/stage.js
@@ -19,42 +19,9 @@ makotokw.Views = makotokw.Views || {};
     },
 
     initialize: function () {
-      this.$mainNavBar = $('#mainNavBar');
-      if (makotokw.isTopPage) {
-        $(window).bind('scroll.stage', _.bind(this.stickyHeader, this));
-        this.$mainNavBar.hide();
-      } else {
-        $(window).bind('scroll.stage', _.bind(this.stickyFooter, this));
-        $(window).bind('resize.stage', _.bind(this.stickyFooter, this));
-        this.$mainNavBar.show();
-        this.stickyFooter();
-      }
-    },
-
-    stickyHeader: function (/*e*/) {
-      if ($(window).scrollTop() > this.stickyHeaderTop) {
-        if (makotokw.isTopPage) {
-          this.$mainNavBar.fadeIn();
-        } else {
-          this.$mainNavBar.fadeOut();
-        }
-      } else {
-        if (makotokw.isTopPage) {
-          this.$mainNavBar.fadeOut();
-        } else {
-          this.$mainNavBar.fadeIn();
-        }
-      }
-    },
-
-    stickyFooter: function (/*e*/) {
-      var $margin = $('#footerMargin');
-      var docHeight = $(document.body).height() - $margin.height();
-      if (docHeight < $(window).height()) {
-        var diff = $(window).height() - docHeight;
-        $margin.height(diff);
-      }
+      $('#mainNavBar').headroom({
+        offset: this.stickyHeaderTop
+      });
     }
-
   });
 })();
