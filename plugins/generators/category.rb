28b530e5e8794bb71f6a731721116108837d59f1
makoto_kw <makoto.kw@gmail.com>
2016-08-16 10:07:41 +0900

    Refactoring templates

--- plugins/generators/category.rb
+++ plugins/generators/category.rb
@@ -1,76 +1,40 @@
 # encoding: utf-8
-#
-# Jekyll category page generator.
-# http://recursive-design.com/projects/jekyll-plugins/
-#
-# Version: 0.1.4 (201101061053)
-#
-# Copyright (c) 2010 Dave Perrett, http://recursive-design.com/
-# Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
-#
-# A generator that creates category pages for jekyll sites.
-#
-# Included filters :
-# - category_links:      Outputs the list of categories as comma-separated <a> links.
-# - date_to_html_string: Outputs the post.date as formatted html, with hooks for CSS styling.
-#
-# Available _config.yml settings :
-# - category_dir:          The subfolder to build category pages in (default is 'categories').
-# - category_title_prefix: The string used before the category name in the page title (default is
-#                          'Category: ').
-#
-# Modified for makotokw.com by Makoto Kawasaki
-
 require 'stringex'
 
 module Jekyll
 
-  # The CategoryIndex class creates a single category page for the specified category.
   class CategoryIndex < Page
 
-    # Initializes a new CategoryIndex.
-    #
-    #  +base+         is the String path to the <source>.
-    #  +category_dir+ is the String path between <source> and the category folder.
-    #  +category+     is the category currently being processed.
     def initialize(site, base, category_dir, category)
       @site = site
       @base = base
       @dir  = category_dir
       @name = 'index.html'
+
       self.process(@name)
-      # Read the YAML data from the layout page.
-      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
+      self.read_yaml(File.join(base, '_layouts'), 'archives_blog_category.html')
       self.data['category']    = category
-      # Set the title for this page.
+
       title_prefix             = site.config['category_title_prefix'] || 'Category: '
       self.data['title']       = "#{title_prefix}#{category}"
-      # Set the meta-description for this page.
       meta_description_prefix  = site.config['category_meta_description_prefix'] || 'Category: '
       self.data['description'] = "#{meta_description_prefix}#{category}"
     end
 
   end
 
-  # The Site class is a built-in Jekyll class with access to global site config information.
   class Site
 
-    # Creates an instance of CategoryIndex for each category page, renders it, and
-    # writes the output to a file.
-    #
-    #  +category_dir+ is the String path to the category folder.
-    #  +category+     is the category currently being processed.
     def write_category_index(category_dir, category)
-      index = CategoryIndex.new(self, self.source, category_dir, category)
-      index.render(self.layouts, site_payload)
-      index.write(self.dest)
-      # Record the fact that this page has been added, otherwise Site::cleanup will remove it.
-      self.pages << index
+      page = CategoryIndex.new(self, self.source, category_dir, category)
+      page.render(self.layouts, site_payload)
+      page.write(self.dest)
+      self.pages << page
     end
 
     # Loops through the list of category pages and processes each one.
     def write_category_indexes
-      if self.layouts.key? 'category_index'
+      if self.layouts.key? 'archives_blog_category'
         dir = self.config['category_dir'] || 'categories'
         self.categories.keys.each do |category|
           self.write_category_index(File.join(dir, category.to_url), category)
@@ -80,11 +44,10 @@ module Jekyll
       else
         raise <<-ERR
 
-
 ===============================================
-Error for category_generator.rb plugin
+Error for generators/category.rb plugin
 -----------------------------------------------
-No 'category_index.html' in source/_layouts/
+No 'archives_blog_category.html' in source/_layouts/
 Perhaps you haven't installed a theme yet.
 ===============================================
 
@@ -94,8 +57,6 @@ ERR
 
   end
 
-
-  # Jekyll hook - the generate method is called by jekyll, and generates all of the category pages.
   class GenerateCategories < Generator
     safe true
     priority :low
@@ -106,17 +67,9 @@ ERR
 
   end
 
-
   # Adds some extra filters used during the category creation process.
   module Filters
 
-    # Outputs a list of categories as comma-separated <a> links. This is used
-    # to output the category list for each post on a category page.
-    #
-    #  +categories+ is the list of categories to format.
-    #
-    # Returns string
-    #
     def category_links(categories)
       categories = categories.sort!.map { |c| category_link c }
 
@@ -130,22 +83,11 @@ ERR
       end
     end
 
-    # Outputs a single category as an <a> link.
-    #
-    #  +category+ is a category string to format as an <a> link
-    #
-    # Returns string
-    #
     def category_link(category)
       dir = @context.registers[:site].config['category_dir']
       "<a class='category' href='/#{dir}/#{category.to_url}/'>#{category}</a>"
     end
 
-    # Outputs the post.date as formatted html, with hooks for CSS styling.
-    #
-    #  +date+ is the date object to format as HTML.
-    #
-    # Returns string
     def date_to_html_string(date)
       '<span class="month">' + date.strftime('%b').upcase + '</span> '
         + date.strftime('<span class="day">%d</span> ')
--- plugins/generators/portfolio_index.rb
+++ plugins/generators/portfolio_index.rb
@@ -26,6 +26,10 @@ module Jekyll
       self.data['portfolios'] = list.reverse
     end
 
+    def index?
+      true
+    end
+
   end
 
   class PortfolioIndexPageGenerator < Generator
@@ -35,14 +39,16 @@ module Jekyll
 
     def generate(site)
 
-      portfolio_tags = YAML.load_file(File.join(site.source, '_fixtures', 'portfolio_tag.yml'))
-      portfolio_tags.each do |t|
-        if !t.has_key?('slug') then
-          slug = t['name'].to_s.downcase.gsub(/(\s|[^\w])/, '')
-          t['slug'] = slug
+      unless site.portfolio_tags then
+        portfolio_tags = YAML.load_file(File.join(site.source, '_fixtures', 'portfolio_tag.yml'))
+        portfolio_tags.each do |t|
+          if !t.has_key?('slug') then
+            slug = t['name'].to_s.downcase.gsub(/(\s|[^\w])/, '')
+            t['slug'] = slug
+          end
         end
+        site.portfolio_tags = portfolio_tags
       end
-      site.portfolio_tags = portfolio_tags
 
       portfolios = YAML.load_file(File.join(site.source, '_fixtures', 'portfolio.yml'))
       portfolios.sort_by! { |p| p['copyright_year'] ? p['copyright_year'] : 2000 }
