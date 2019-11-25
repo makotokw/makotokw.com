6a72a0c3c5a8dade443f764eaa9587d0ffd41f32
makoto_kw <makoto.kw@gmail.com>
2019-11-25 16:01:28 +0900

    portfolios.yaml to portfolio template data json

---
+++ build/scripts/fixture-portfolio-to-template-data.js
@@ -0,0 +1,27 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
+
+const fixturePath = path.resolve(__dirname, '../../src/assets/fixtures/portfolios.yml');
+const portfolios = yaml.safeLoad(fs.readFileSync(fixturePath, 'utf8'));
+const portfoliosPageDir = path.resolve(__dirname, '../../src/site/_portfolios');
+
+portfolios.forEach(function (portfolio) {
+  if (!portfolio.url) {
+    return;
+  }
+  const matched = portfolio.url.match(/^\/(|ja\/)portfolio\/(.*)\/(.*)\//);
+  if (!matched) {
+    return;
+  }
+  const [, , category, page] = matched;
+
+  ['', '.ja'].forEach(function (categorySuffix) {
+    const pagePath = `${portfoliosPageDir}/${category}${categorySuffix}/${page}.md`;
+    if (!fs.existsSync(pagePath)) {
+      return;
+    }
+    const dataPath = pagePath.replace(/\.md$/, '.11tydata.json');
+    fs.writeFileSync(dataPath, JSON.stringify({ portfolio }, null, null));
+  });
+});
